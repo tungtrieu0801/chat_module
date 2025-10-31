4edce33518a86663e2d7705847ae796d471d37da
tungtt <tungtt2@vnpost.vn>
2025-10-31 16:46:27 +0700

    remove log

--- src/middleware/socket.middleware.ts
+++ src/middleware/socket.middleware.ts
@@ -3,14 +3,10 @@ import { validateJwtToken } from 'src/helpers/jwt.helper';
 export function socketMiddleware(secret: string) {
     return ( socket: Socket, next: (err?: any) => void ) => {
         const token = socket.handshake.auth.token as string;
-        console.log("Token received:", token);
-        console.log("Socket middleware called");
         const payload = validateJwtToken(token);
         if(!payload) {
-            console.log("Invalid token"); 
             next(new Error('Authentication error: Invalid token'));
         } else {
-            console.log("Payload:", payload);
             (socket as any).userId = payload.sub;
             (socket as any).username = payload.username;
             console.log("id la: ", (socket as any).userId)
