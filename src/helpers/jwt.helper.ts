958b51f816cb2ad884b41724c8a2683afbabbecb
tungtt <tungtt2@vnpost.vn>
2025-11-17 16:53:29 +0700

    refactor: refactor code connect socket

--- src/helpers/jwt.helper.ts
+++ src/helpers/jwt.helper.ts
@@ -2,8 +2,12 @@ import { JwtService } from '@nestjs/jwt';
 import * as jwt from 'jsonwebtoken';
 
 export function validateJwtToken(token: string): Record<string, any> | null {
+  const JWT_SECRET = process.env.JWT_SECRET;
   try {
-    return jwt.verify(token, '123456789099') as Record<string, any>;
+    if (!JWT_SECRET) {
+      throw new Error('JWT_SECRET not set');
+    }
+    return jwt.verify(token, JWT_SECRET) as Record<string, any>;
   } catch (error) {
     return null;
   }
